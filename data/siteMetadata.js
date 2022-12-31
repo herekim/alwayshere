89decd48ab11d29c984deaa943c8dda8c31a88f7
alwayshere <76519867+herekim@users.noreply.github.com>
2022-12-31 22:30:44 +0900

    modify banner

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/herekim/alwayshere',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/logo.png',
   email: 'khere220@gamil.com',
   github: 'https://github.com/herekim',
   // twitter: 'https://twitter.com/Twitter',
