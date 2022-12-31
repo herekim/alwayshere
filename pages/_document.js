0724bc2551b0b4e3d60de21e0bfde823e23d5825
alwayshere <76519867+herekim@users.noreply.github.com>
2023-01-01 02:17:10 +0900

    modify giscus

--- pages/_document.js
+++ pages/_document.js
@@ -4,21 +4,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="scroll-smooth">
         <Head>
-          {/* <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/static/favicons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/static/favicons/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/static/favicons/site.webmanifest" />
-          <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" /> */}
           <link rel="icon" type="image/png" href="/static/favicons/keyboard.png" />
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
@@ -30,7 +15,7 @@ class MyDocument extends Document {
             data-repo-id="R_kgDOIszcKg"
             data-category="[ENTER CATEGORY NAME HERE]"
             data-category-id="[ENTER CATEGORY ID HERE]"
-            data-mapping="title"
+            data-mapping="pathname"
             data-strict="0"
             data-reactions-enabled="1"
             data-emit-metadata="1"
