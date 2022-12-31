88e3abfe00dc34e55a0f536b505c5d79f78a336a
alwayshere <76519867+herekim@users.noreply.github.com>
2022-12-31 22:24:43 +0900

    add giscus

--- pages/_document.js
+++ pages/_document.js
@@ -24,6 +24,23 @@ class MyDocument extends Document {
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+          <script
+            src="https://giscus.app/client.js"
+            data-repo="herekim/alwayshere"
+            data-repo-id="R_kgDOIszcKg"
+            data-category="Announcements"
+            data-category-id="DIC_kwDOIszcKs4CTV0c"
+            data-mapping="pathname"
+            data-strict="0"
+            data-reactions-enabled="1"
+            data-emit-metadata="1"
+            data-input-position="top"
+            data-theme="preferred_color_scheme"
+            data-lang="ko"
+            data-loading="lazy"
+            crossOrigin="anonymous"
+            async
+          ></script>
         </Head>
         <body className="bg-white font-sans text-black antialiased dark:bg-gray-900 dark:text-white">
           <Main />
